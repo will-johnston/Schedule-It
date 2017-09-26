e30a870c16e934799fdd8212c7e03651b8d0d563
Will <johnstw@purdue.edu>
2017-09-26 04:50:18 +0000

    seperated connecting and query/executing into seperate files, added add user to db file

---
+++ backend/src/database/AddUserToDb.java
@@ -0,0 +1,53 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;		
+import javax.sql.DataSource;
+		
+public class AddUserToDb {
+	public static void main(String[] args) {
+		for (String str: args) {
+			System.out.println(str);
+		}
+		String id = (args[0]);
+		String username = args[1];
+		String fullname = args[2];
+		String password = args[3];
+		String email = args[4];
+		String phoneNumber = args[5];		
+		addUser(id, username, fullname, password, email, phoneNumber);
+	}
+				
+	private static void addUser(String id, String username, String fullname, String password, String email,
+	String phoneNumber) {
+		DataSource ds = null;
+		ds = data_source_factory.get_data_source();
+		if (ds == null) {
+			System.out.println("null data source");
+			return;
+		}					
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet result = null;
+		try {
+			connection = ds.getConnection();
+			String query = "INSERT INTO Users VALUES('" + id +  "','" + username + "','"  + fullname + "','" + password + "','" + email + "','" + phoneNumber + "')";
+			System.out.println(query);
+			statement = connection.createStatement();
+			int ex = statement.executeUpdate(query);
+			/*while(result.next()){
+				System.out.println(result.getStatement());       
+			}*/
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(result != null) result.close();
+				if(statement != null) statement.close();
+				if(connection != null) connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ backend/src/database/data_source_factory.java
@@ -0,0 +1,32 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+import javax.sql.DataSource;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+/**
+ * Created by williamjohnston on 9/22/17.
+*/
+public class data_source_factory {
+
+    public static DataSource get_data_source() {
+	    Properties properties = new Properties();
+	    MysqlDataSource ds = null; 
+	    FileInputStream fis = null;
+	    try { 
+		ds = new MysqlDataSource();
+		fis = new FileInputStream("/home/will/db.properties");
+		properties.load(fis);
+		
+		ds.setServerName(properties.getProperty("DB_URL")); 
+		ds.setUser(properties.getProperty("DB_USERNAME"));
+		ds.setPassword(properties.getProperty("DB_PASSWORD"));
+		ds.setPortNumber(Integer.parseInt(properties.getProperty("DB_PORT")));
+		ds.setDatabaseName(properties.getProperty("DB_NAME"));
+	 }
+	 catch(IOException e) {
+	 	e.printStackTrace();
+	 }
+	 return ds;
+    }
+}
