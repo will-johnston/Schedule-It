cbc4b148990362aa23a97e3b6eb3bd2e7b603c33
Kyle Burke <kyle.n.burke@gmail.com>
2017-10-05 22:49:03 -0400

    Account settings (#72)
    * Continued work on get user settings
    
    * User account settings can be changed
    
    * Fixed some more stuff git broke

--- app/resources/scriptMain.js
+++ app/resources/scriptMain.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	//variables
-	var numOfGroups = 1;
+	var cookie = -1035393116;
 
 	var assignFunctionality = function() {
 		$("#vPillsTab a").on("shown.bs.tab", function(event) {
@@ -24,37 +24,98 @@ $(document).ready(function(){
 		});
 	};
 
-	var ajax = function(method, url, data, onSuccess, onFail) {
-		$.ajax({
-			method: method,
-			url: url,
-			data: data,
-			datatype: "jsonp"
-		}).done(onSuccess(result)).fail(onFail(result));
+	var accessServer = function(method, url, data, onSuccess, onFail) {
+		var xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+		xhr.onload = function () {
+			if (xhr.status === 200)
+				onSuccess(xhr.response);
+			else
+				onFail(xhr.response);
+		};
+
+		xhr.send(data);
 	};
 
 	//SETTINGS MODAL
+	var fullName;
+	var username;
+	var email;
+	var phoneNumber;
 	$("#accountSettingsButton").click(function() {
 		//populate the account settings modal fields
-		var fullName;
-		var username;
-		var email;
-		var phoneNumber;
-		var changePassword;
-		var confirmPassword;
-		var picURL;
 
-		$("#settingsModalFullNameField").val(fullName);
-		$("#settingsModalUsernameField").val(username);
-		$("#settingsModalEmailField").val(email);
-		$("#settingsModalPhoneNumberField").val(phoneNumber);
-		$("#settingsModalChangePasswordField").val(changePassword);
-		$("#settingsModalConfirmPasswordField").val(confirmPassword);
-		$("#settingsModalPicture").attr("src", picURL);
+		//cookie = document.cookie;
+		var data = JSON.stringify({cookie: cookie});
 
+		accessServer("POST", "http://scheduleit.duckdns.org/api/user/getsettings", data,
+			function(result) { //success
+				var json = JSON.parse(result);
+
+				fullName = json.fullname;
+				username = json.username;
+				email = json.email
+				phoneNumber = json.phone;
+
+				$("#settingsModalFullNameField").val(fullName);
+				$("#settingsModalUsernameField").val(username);
+				$("#settingsModalEmailField").val(email);
+				$("#settingsModalPhoneNumberField").val(phoneNumber);
+				$("#settingsModalChangePasswordField").val("");
+				$("#settingsModalConfirmPasswordField").val("");
+				$("#settingsModalPicture").attr("src", "resources/profileDefaultPhoto.png");
+			},
+			function(result) { //fail
+				alert("Failed to obtain user account settings");
+				console.log(result);
+			});
 	});
 	$("#accountSettingsModalSaveButton").click(function() {
-		
+		var fullNameChanged = $("#settingsModalFullNameField").val();
+		var emailChanged = $("#settingsModalEmailField").val();
+		var phoneNumberChanged = $("#settingsModalPhoneNumberField").val();
+		var passwordChanged = $("#settingsModalChangePasswordField").val();
+		var confirmPasswordChanged = $("#settingsModalConfirmPasswordField").val();
+		//var picURL = ...
+
+		var data = {};
+		data["username"] = username;
+		data["cookie"] = cookie;
+
+		if(fullName != fullNameChanged) {
+			data["fullname"] = fullNameChanged;
+		}
+
+		if(email != emailChanged) {
+			data["email"] = emailChanged;
+		}
+
+		if(phoneNumber != phoneNumberChanged) {
+			data["phone"] = phoneNumberChanged;
+		}
+
+		if(passwordChanged != "") {
+			if(passwordChanged == confirmPasswordChanged) {
+				data["pass"] = passwordChanged;
+			}
+			else {
+				alert("Passwords do not match");
+				return;
+			}
+		}
+
+		console.log(data);
+
+		accessServer("POST", "http://scheduleit.duckdns.org/api/user/edit", JSON.stringify(data),
+			function(result) { //success
+				console.log("Successfully changed user account settings");
+				$("#accountSettingsModal").modal("hide");
+			},
+			function(result) { //fail
+				alert("Failed to change user account settings");
+				console.log(result);
+			});
+
 	});
 	$("#accountSettingsModalDeleteAccountButton").click(function() {
 		
@@ -94,20 +155,6 @@ $(document).ready(function(){
 	$("#groupSettingsButton").click(function() {
 		//populate the group settings modal with group information
 
-		cookie = document.cookie;
-		console.log(cookie);
-
-		ajax("GET", "http://scheduleit.duckdns.org/api/user/getsettings", 
-			JSON.stringify({cookie: cookie}),
-			function(result) {
-				console.log("success");
-				console.log(result);
-			},
-			function(result) {
-				console.log("failed");
-				console.log(result);
-			});
-
 	});
 	$("#groupSettingsSaveButton").click(function() {
 		//Write the changed values to the database
@@ -121,7 +168,7 @@ $(document).ready(function(){
 		//Call the create group endpoint with parameters
 
 
-		var id = "group" + ++numOfGroups + "Content";
+		var id = "group" + ++$("vPillsContent").childre().length + "Content";
 
 		var tabHTML = `<a class="nav-link" data-toggle="pill" href="#` + id + `" role="tab">` + name + `</a>`;
 		
