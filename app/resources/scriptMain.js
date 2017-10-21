9b32d18be5cc9978bbbf641b89efa0e74d6612d0
KyleNBurke <kyle.n.burke@gmail.com>
2017-10-21 18:30:53 -0400

    Made groups settinsg button hidden by default

--- app/resources/scriptMain.js
+++ app/resources/scriptMain.js
@@ -437,7 +437,7 @@ $(document).ready(function(){
 							<img src="resources/groupDefaultPhoto.jpg" alt="Default Group Photo" class="img-thumbnail" width="100">
 							<h3>` + name + `</h3>
 							<p>` + info + `</p>
-							<button type="button" class="btn btn-secondary btn-sm" id="groupSettingsButton" data-toggle="modal" data-target="#groupSettingsModal">Group settings</button>
+							<button type="button" class="btn btn-secondary btn-sm invisible" id="groupSettingsButton" data-toggle="modal" data-target="#groupSettingsModal">Group settings</button>
 						</div>
 					</div>
 				</div>
