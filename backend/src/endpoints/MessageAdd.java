d0fb3d1c66d8d5341d7e1ea8e82ee45b1f20208d
Craig Ruble <cruble@purdue.edu>
2017-10-26 21:21:24 -0400

    Changed endpoint success message

--- backend/src/endpoints/MessageAdd.java
+++ backend/src/endpoints/MessageAdd.java
@@ -29,8 +29,7 @@ public class MessageAdd implements IAPIRoute {
                 //Have username, groupID, timestamp, and message
                 Messages message = new Messages();
                 boolean ret = message.setMessage(args);
-                Socketeer.send(HTTPMessage.makeResponse("{\"error\":\"Returned fine\"}\n", HTTPMessage.HTTPStatus.OK, HTTPMessage.MimeType.appJson, true), sock);
-
+                Socketeer.send(HTTPMessage.makeResponse("{\"no error\":\"Message set\"}\n", HTTPMessage.HTTPStatus.OK, HTTPMessage.MimeType.appJson, true), sock);
 
                 return;
             }
