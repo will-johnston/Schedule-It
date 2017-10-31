ad58e52e29bdfb8b8af31c1801c2ab9ce69bdef5
Ryan Everett <ryaneverett33@outlook.com>
2017-10-31 16:05:34 -0400

    Don't be dumn

--- backend/src/Main.java
+++ backend/src/Main.java
@@ -9,6 +9,8 @@ public class Main {
             Router router = new Router();
             //Images images = new Images();
             Tracker tracker = new Tracker();
+            NotificationHandler handler = new NotificationHandler(tracker);
+            NotificationDealer dealer = new NotificationDealer(tracker, handler);
             //upload up = new upload(images, tracker);           //For special upload case
             //router.add("/upload", up);
             //router.add("/upload/chunk", up);
@@ -16,20 +18,28 @@ public class Main {
             router.add("/example/example", new ExampleEndpoint());
             router.add("/user/login", new UserLogin(tracker));
             router.add("/user/edit", new UserEdit(tracker));
-            router.add("/user/groups/calendar/get", new GroupGetCalendar(tracker));
-            router.add("/user/groups/calendar/add", new GroupAddCalendar(tracker));
-            //router.add("/user/calendar/get", new UserGetCalendar(tracker));
-            //router.add("/user/calendar/add", new UserAddCalendar(tracker));
             router.add("/user/groups/leave", new GroupLeave(tracker));
-            router.add("/user/groups/join", new GroupJoin());
-            router.add("/user/search", new UserSearch());
-            router.add("/user/groups/add", new GroupAdd());
+            //router.add("/user/groups/join", new GroupJoin());
+            //router.add("/user/search", new UserSearch());
+            //router.add("/user/groups/add", new GroupAdd());
             router.add("/user/groups/edit", new GroupEdit());
             router.add("/user/groups/create", new GroupCreate(tracker));
+            router.add("/user/groups/chat", new MessageAdd(tracker));
             router.add("/user/friends/remove", new FriendsRemove(tracker));
             router.add("/user/create", new UserCreate(tracker));
             router.add("/user/friends/add", new FriendsAdd(tracker));
-            return router;
+            router.add("/user/friends/invite", new FriendInvite(tracker, handler));
+            router.add("/user/getsettings", new UserGetSettings(tracker));
+            router.add("/user/friends/get", new FriendsGet(tracker));
+            router.add("/user/notifications/get", dealer);
+            router.add("/user/notifications/respond", dealer);
+            //router.add("/user/notifications/dismiss", dealer);
+            router.add("/user/groups/get", new GroupGet(tracker));
+            router.add("/user/groups/invite", new GroupInvite(tracker, handler));
+            router.add("/timeinput/add", new TimeInputAdd(tracker));
+            router.add("/user/groups/calendar/get", new GroupGetCalendar(tracker));
+            router.add("/user/groups/calendar/add", new GroupAddCalendar(tracker));
+	    return router;
         }
         catch (Exception e) {
             System.out.println("Init server.Router encountered an error!");
