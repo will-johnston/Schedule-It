aaf570e67b522c2d85b39ce70b90cbd09bccffaf
Ryan Everett <ryaneverett33@outlook.com>
2017-11-12 12:36:22 -0500

    make sure shit compiles

--- backend/tests/LoggedInTest.java
+++ backend/tests/LoggedInTest.java
@@ -4,7 +4,7 @@ import java.util.concurrent.*;
 public class LoggedInTest {
     public static void main(String[] args) throws Exception {
         Tracker tracker = new Tracker();
-        User user = new User("bob", "bob@aol.com", "asdf", "1234567890", 0, null, null);
+        User user = new User("bob", "bob@aol.com", "asdf", "1234567890", 0, null, null, null);
         int cookie = tracker.login(user);
         System.out.println("Logged in? " + tracker.isLoggedIn(cookie));
         Thread.sleep(60 * 1000);
